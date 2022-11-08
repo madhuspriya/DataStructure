96cacd55ddfc3bf5a2a46faa59993a784c2b250c
madhuspriya <madhuspriyas.stacksum@gmail.com>
2022-11-08 17:51:49 +0530

    reverse array

---
+++ Program_Array/src/main/java/com/example/array/ProgramArrayApplication.java
@@ -0,0 +1,13 @@
+package com.example.array;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramArrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramArrayApplication.class, args);
+	}
+
+}
